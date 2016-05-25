bfcf77a990737653e40d8a05e5ac3d0b9bfa79d2
j <j.gao@aaa.net>
2016-05-25 13:24:14 +0100

    run BATMAN R script using package 'optparse'

--- runBATMAN.R
+++ runBATMAN.R
@@ -1,83 +1,102 @@
-#!/usr/bin/Rscript
+#!/usr/bin/env Rscript
 
-## Collect arguments
-args <- commandArgs(TRUE)
- 
-## Display help information when no enough arguments passed
-if(length(args) < 4) {
-  args <- c("--help")
+## check if "optparse" is installed
+package_installed<-require("optparse")
+print(package_installed)
+## install the package "optparse" if it wasn't installed.
+if (!package_installed)
+{
+   install.packages("optparse", repos="http://cran.us.r-project.org")
 }
 
-## Help section
-if("--help" %in% args) {
-  cat(" 
-      The R Script
- 
-      Arguments:
-      arg1: someValue   - input NMR data
-      arg2: someValue   - output directory
-      --help            - print this text
- 
-      Example:
-      ./test.R -i \"input NMR data\" -o \"output directory(for example: \"C:/temp\") \"  \n\n")
- 
-  q(save="no")
-}
+## import optparse library
+suppressPackageStartupMessages(library("optparse"))
 
-## Parse arguments -- for import NRM data
-if (args[1]=="-i") {
-   batmanData<-args[2]
-} else if (args[3]=="-i") {
-   batmanData<-args[4]
-}
-# Path '\' in arguments will be read as '\\', needs to be replaced by '/'
-backslash <- grepl('\\\\',batmanData)
-sep <- if(backslash) '\\\\' else '/'
-tempPath <-as.list(strsplit(batmanData,sep)[[1]])
-if (backslash) {
-   i<-1
-   path<-paste(tempPath[1],"/",sep="")
-   i<-2
-   while (i<length(tempPath)) {
-      path1temp<-paste(tempPath[i],"/", sep="")
-      path<-paste(path,path1temp,sep="")
-      i<-i+1
+## specify our desired options in a list
+## by default OptionParser will add an help option equivalent to
+## make_option(c("-h", "--help"), action="store_true", default=FALSE,
+## help="Show this help message and exit")
+option_list <- list(
+   make_option(c("-i", "--inputData"), help="Full path to the input NMR spectra data"),
+   make_option(c("-o", "--output"), help="Export BATMAN results to your specified folder"),
+   make_option(c("-p", "--batOptions"), help="Upload BATMAN options, if there is no this option, using the default BATMAN options"),
+   make_option(c("-u", "--multiData"), help="Upload user's metabolites template, if there is no this option, using the default template"),
+   make_option(c("-l", "--metaList"), help="Upload a list of wanted metabolites, if there is no this option, using the default metabolites list")
+)
+
+# get command line options, if help option encountered print help and exit,
+# otherwise if options not found on command line then set defaults,
+opt <- parse_args(OptionParser(option_list=option_list))
+
+## function of replacing "\" to "/" because "\" is interpreted as "\\" 
+## parsing the arguments
+replaceBSlash<-function(paths)
+{
+   # Path '\' in arguments will be read as '\\', needs to be replaced by '/'
+   backslash <- grepl('\\\\',paths)
+   sep <- if(backslash) '\\\\' else '/'
+   tempPath <-as.list(strsplit(paths,sep)[[1]])
+   if (backslash) {
+      i<-1
+      path<-paste(tempPath[1],"/",sep="")
+      i<-2
+      while (i<length(tempPath)) {
+         path1temp<-paste(tempPath[i],"/", sep="")
+         path<-paste(path,path1temp,sep="")
+         i<-i+1
+      }
+     path1temp<-paste(tempPath[i], sep="")
+     path<-paste(path,path1temp,sep="")
+     newPath<-path
+   } else {
+     newPath<-paths
    }
-  path1temp<-paste(tempPath[i], sep="")
-  path<-paste(path,path1temp,sep="")
-  batmanData<-path
 }
 
-## Parse arguments -- for export BATMAN test resutls to a folder on local drive
-if (args[1]=="-o") {
-   cpbatmanOutputDir<-args[2]
-} else if (args[3]=="-o") {
-   cpbatmanOutputDir<-args[4]
+opt$inputData<-replaceBSlash(opt$inputData)
+opt$output<-replaceBSlash(opt$output)
+
+## copy the options, metabolites template and list to the BATMAN
+## input folder is the files are provided
+
+if (is.null(opt$batOptions)) {
+   print("using default BATMAN options")
+} else {
+   opt$batOptions<-replaceBSlash(opt$batOptions)
 }
-# Path '\' in arguments will be read as '\\', needs to be replaced by '/'
-backslash <- grepl('\\\\',cpbatmanOutputDir)
-sep <- if(backslash) '\\\\' else '/'
-tempPath <-as.list(strsplit(cpbatmanOutputDir,sep)[[1]])
-if (backslash) {
-   i<-1
-   path<-paste(tempPath[1],"/",sep="")
-   i<-2
-   while (i<length(tempPath)) {
-        path1temp<-paste(tempPath[i],"/", sep="")
-        path<-paste(path,path1temp,sep="")
-        i<-i+1
+
+if (is.null(opt$multiData)) {
+   print("using default BATMAN metabolites template")
+} else {
+   opt$multiData<-replaceBSlash(opt$multiData)
+}
+
+if (is.null(opt$metaList)) {
+   print("using default BATMAN metabolites list")
+} else {
+   opt$metaList<-replaceBSlash(opt$metaList)
+}
+
+batmanInputDir<-paste(getwd(), "/runBATMAN/BatmanInput", sep="")
+if (dir.exists(batmanInputDir)) {
+   if (!is.null(opt$batOptions)) {
+      file.copy(opt$batOptions, batmanInputDir, overwrite = TRUE)
    }
-   path1temp<-paste(tempPath[i], sep="")
-   path<-paste(path,path1temp,sep="")
-   cpbatmanOutputDir<-path
-}
+   if (!is.null(opt$multiData)) {
+      file.copy(opt$multiData, batmanInputDir, overwrite = TRUE)
+   }
+   if (!is.null(opt$metaList)) {
+      file.copy(opt$metaList, batmanInputDir, overwrite = TRUE)
+   }   
+} 
+
 ## Run BATMAN
 library(batman)
-bm<-batman(txtFile=batmanData)
+bm<-batman(txtFile=opt$inputData)
 ## Read BATMAN results path
 resultsDir<-paste(bm$outputDir)
 ## Copy BATMAN results to the specified folder
 list.of.files<-list.files(resultsDir, full.names=TRUE)
-file.copy(list.of.files,cpbatmanOutputDir)
+file.copy(list.of.files,opt$output)
 
 q(save="no")
