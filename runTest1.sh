c25b34a8d9b0370afc3297815ff46e7515126d6f
j <j.gao@aaa.net>
2016-12-01 13:49:33 +0000

    changed searching output folder

--- runTest1.sh
+++ runTest1.sh
@@ -43,9 +43,9 @@ wget https://raw.githubusercontent.com/jianlianggao/batman/develop/test_data/res
 
 #copy output files to specified folder for comparison
 # find BATMAN output folder
-found_subfolders=($(/bin/ls -1 -R | grep '^[0-9]\{2\}_[A-Z,a-z]\{3\}_.*$'))
+found_subfolders=($(/bin/ls -R | grep '^[0-9]\{2\}_[A-Z,a-z]\{3\}_.*$'))
 output_path="runBATMAN/BatmanOutput/$found_subfolders"
-output_path="results"
+#output_path="results"
 
 
 #run comparison
